0518c1ed5c0d64ba2e96452900696fefaa1923c2
Aryan <aryanguenthner@users.noreply.github.com>
2026-06-03 18:24:17 -0700

    Updated

--- torghost-start.sh
+++ torghost-start.sh
@@ -2,7 +2,8 @@
 
 echo "Darkweb you asked for it"
 echo
-exec /usr/bin/torghostng -s -id nl
+sudo LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libcrypto.so.3 torghostng -s -id cz,nl
+# exec /usr/bin/torghostng -s -id nl
 
 # sudo visudo Notes
 # kali ALL=(ALL) NOPASSWD: /root/torghost-start.sh, /root/torghost-stop.sh
