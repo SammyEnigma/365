2ffbf470d2be798148d6f9dc50e1de8d0d3e1cb8
Aryan <aryanguenthner@users.noreply.github.com>
2026-09-06 21:57:10 -0700

    Add files via upload

--- torghost-start.sh
+++ torghost-start.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-
-echo "Darkweb you asked for it"
+echo
+echo "Darkweb Access Granted"
 echo
 sudo LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libcrypto.so.3 torghostng -s -id cz,nl
-# exec /usr/bin/torghostng -s -id nl
+python3 /opt/TorghostNG/torghostng.py -s -id cz,nl
 
 # sudo visudo Notes
 # kali ALL=(ALL) NOPASSWD: /root/torghost-start.sh, /root/torghost-stop.sh
+echo
 
+echo -e "\e[031mDarkweb Network Information\e[0m"
+echo "---------------------------------"
+printf "| %-12s | %-20s |\n" "Public IP" "$EXT"
+printf "| %-12s | %-20s |\n" "Country" "$COUNTRY"
+printf "| %-12s | %-20s |\n" "State" "$REGION"
+printf "| %-12s | %-20s |\n" "City" "$CITY"
+printf "| %-12s | %-20s |\n" "Kali IP" "$KALI"
+echo "---------------------------------"
+echo
--- torghost-stop.sh
+++ torghost-stop.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Exiting the Darkweb you asked for it"
+echo "Exiting the Darkweb"
 echo
-exec /usr/bin/torghostng -x
-
+# exec /usr/bin/torghostng -x
+python3 /opt/TorghostNG/torghostng.py -x
+echo
+echo
+echo -e "\e[031mNetwork Information\e[0m"
+echo "---------------------------------"
+printf "| %-12s | %-20s |\n" "Public IP" "$EXT"
+printf "| %-12s | %-20s |\n" "Country" "$COUNTRY"
+printf "| %-12s | %-20s |\n" "State" "$REGION"
+printf "| %-12s | %-20s |\n" "City" "$CITY"
+printf "| %-12s | %-20s |\n" "Kali IP" "$KALI"
+echo "---------------------------------"
+echo
